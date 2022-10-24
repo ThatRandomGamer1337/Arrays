746033d3fd09eac012cfa06289e72caba337088f
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-10-24 11:40:38 -0300

    Initial commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Arrays");
+            Console.WriteLine();
+
+
+            // weapons
+            // 0 = pistol
+            // 1 = shotgun
+            // 2 = rifle
+            // 3 = sniper
+            // 4 = rocket launcher
+            int weapon;
+            weapon = 0;
+
+            // ammo array
+            int[] ammo;
+            ammo = new int[5];
+            ammo[0] = 6; // pistol
+            ammo[1] = 8; // shotgun
+            ammo[2] = 30; // rifle
+            ammo[3] = 5; // sniper
+            ammo[4] = 10; // rocket launcher
+
+
+            // simulated gameplay
+            Console.WriteLine(ammo[weapon]);
+            weapon = 3;
+            Console.WriteLine(ammo[weapon]);
+
+            Console.ReadKey(true);
+
+        }
+
+        static void ShowCurrentWeaponAmmo()
+        {
+
+        }
+
+
+
+    }
+}
